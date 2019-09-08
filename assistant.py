cadc725a7729d4fb6f0e5ad7e5695257ea43d9d6
k3nnet <kkmphele@gmail.com>
2019-09-08 09:41:19 +0200

    created a talk method to allow the assistant to speak

--- assistant.py
+++ assistant.py
@@ -1,3 +1,16 @@
 from gtts import  gTTS
 from pygame import mixer
 import speech_recognition as sr
+
+
+
+#function to let the assistant speak  
+def talk(audio):
+    print(audio)
+    for line in audio.splitlines():
+        text_to_speech=gTTS(text=audio,lang='en-uk')
+        text_to_speech.save('audio.mp3')
+        mixer.init()
+        mixer.music.load("audio.mp3")
+        mixer.music.play()
+    
